5d17c925296a9677b70f42a9152c35805debd67c
Ben Konicek <ben.konicek@gmail.com>
2019-09-03 02:15:17 +0000

    part 3 - add or delete users

--- bash/users.sh
+++ bash/users.sh
@@ -1,39 +1,50 @@
 #!/bin/bash
 
-USERNAME=$1
-PASSWORD=$2
+USERNAME=$2
+PASSWORD=$3
 
-# simple bash script demonstrating arguments
+addUser()
+{
 
-EXIT_CODE=$?
+    # If no password specified in arguments, generate a random password
+    if [ -z $PASSWORD ]
+    then
+        PASSWORD=$(date +%s | sha256sum | base64 | head -c 32)
+    fi
 
-# If no password specified in arguments, generate a random password
-if [ -z $PASSWORD ]
-then
-    PASSWORD=$(date +%s | sha256sum | base64 | head -c 32)
-fi
+    # create a text file with username and password, then create the user and set the password
+    echo "Hello $USERNAME, here are your credentials to log in." >> credentials.txt
+    echo "" >> credentials.txt
+    echo $USERNAME >> credentials.txt
+    echo $PASSWORD >> credentials.txt
 
-# create a text file with username and password, then create the user and set the password
-echo "Hello $USERNAME, here are your credentials to log in." >> credentials.txt
-echo "" >> credentials.txt
-echo $USERNAME >> credentials.txt
-echo $PASSWORD >> credentials.txt
+    useradd -m $USERNAME
 
-useradd -m $USERNAME
+    echo $USERNAME:$PASSWORD | chpasswd
 
-echo $USERNAME:$PASSWORD | chpasswd
+    # mail credentials file to user and then delete it
+    mail -A ./credentials.txt -s "Here are your credentials" $USERNAME@ourcooltechcompany.com < /dev/null
+    rm -rf ./credentials.txt
 
-# mail credentials file to user and then delete it
-mail -A ./credentials.txt -s "Here are your credentials" $USERNAME@ourcooltechcompany.com < /dev/null
-rm -rf ./credentials.txt
+    # Copy rules file into user's home directory
+    cp ./rules.txt /home/$USERNAME/
 
-# Copy rules file into user's home directory
-cp ./rules.txt /home/$USERNAME/
-
-# Send success message to user or to person running script
-if [ $EXIT_CODE -eq 0 ]
-then
+    # Send success message to user or to person running script
     echo "Successfully created user $USERNAME!"
+}
+
+deleteUser()
+{
+    userdel -r $USERNAME
+    echo "$USERNAME has been deleted"
+}
+
+if [ $1 == "add" ]
+then
+    addUser
+elif [ $1 == "remove" ]
+then
+    deleteUser
 else
-    echo "Could not create user $USERNAME"
+    echo "You need to specify either 'add' or 'remove'."
 fi
